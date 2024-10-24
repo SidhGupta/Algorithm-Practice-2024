c9cbf8bd97eaeeb98edebda3784d95cbcc3876bb
SidhGupta <158204477+SidhGupta@users.noreply.github.com>
2024-10-24 11:42:54 -0400

    zczxc

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-function factorial(n){
-    //garud clause
-    if (n <= 0) {
-        return "negative number or 0"
+// function factorial(n){
+//     //garud clause
+//     if (n <= 0) {
+//         return "negative number or 0"
+//     }
+//     let result = 1;
+//     for(let i = 1; i <= n; i++){
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// factorial(6)
+
+
+let yesterday = []
+let today = []
+
+function occupiedParking(spaces, yesterday, today){
+    let count = 0;
+    for(let i = 0; i <spaces; i++) {
+        if (yesterday[i] === "C" && today[i] === "c"){
+            count++;
+        }
     }
-    let result = 1;
-    for(let i = 1; i <= n; i++){
-        result *= i;
-    }
-    return result;
+    return count;
 }
 
-factorial(6)
+
+console.log(occupiedParking(5, "" , ""));
